3b8e92a77485844ccf001c3db4ecc1662434fbc6
Bogdan <zarechnyy00@internet.ru>
2025-09-29 23:09:42 +0300

    create navigation animation

---
+++ app/components/header/header.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Header() {
+  const [statusMenu, setStatusMenu] = useState(false);
+
+  const openMenu = () => {
+    setStatusMenu(!statusMenu);
+  };
+
+  return (
+    <header className="w-5xl m-auto">
+      <div className="py-5 px-3 flex items-center relative">
+        <nav
+          className={`py-5 px-3 absolute top-0 left-0 w-full flex justify-between backdrop-blur-sm duration-500 ${
+            statusMenu ? 'translate-y-0' : '-translate-y-full'
+          }`}
+        >
+          <ul className="flex justify-between w-3xs">
+            <li>Продукия</li>
+            <li>Партнерам</li>
+            <li>Контакты</li>
+          </ul>
+          <button
+            className="text-lg font-medium cursor-pointer"
+            onClick={openMenu}
+          >
+            Close nav
+          </button>
+        </nav>
+        <div className="flex-1 flex justify-center">
+          <div className="text-3xl font-semibold">LogoHan</div>
+        </div>
+        <button
+          className="text-lg font-medium cursor-pointer"
+          onClick={openMenu}
+        >
+          Open nav
+        </button>
+      </div>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
+import Header from './components/header/header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,10 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>
+        <Header></Header>
+        {/* <nav>
           <Link href="/">Home</Link>
           <Link href="/blog">Blog</Link>
-        </nav>
+        </nav> */}
         {children}
       </body>
     </html>
